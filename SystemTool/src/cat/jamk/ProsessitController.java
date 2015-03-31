47f71ccf6510f10d0d7d7ad061cd09e6cf5f0e11
laurima <h9142@OHJE-330-WS-02.Labranet.jamk.fi>
2015-03-31 14:53:27 +0300

    omat fxml ja controllerit välilehdille

---
+++ SystemTool/src/cat/jamk/ProsessitController.java
@@ -0,0 +1,30 @@
+package cat.jamk;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author h9142
+ */
+public class ProsessitController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
