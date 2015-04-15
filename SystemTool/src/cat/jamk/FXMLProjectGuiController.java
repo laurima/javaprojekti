6edeef0058becb065b3fac39b966a4e114c79ea0
Lauri Mäkinen <lauri.m.makinen@gmail.com>
2015-04-16 00:27:35 +0300

    Tietojen näyttö guissa
    Lisätty tarvittavat muuttujat yms. synccaan windowsin puolelle, että
    näkee toimiiko kaikki ominaisuudet kun mac ei tykkää :S

--- SystemTool/src/cat/jamk/FXMLProjectGuiController.java
+++ SystemTool/src/cat/jamk/FXMLProjectGuiController.java
@@ -7,7 +7,6 @@ package cat.jamk;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
@@ -16,10 +15,39 @@ import javafx.scene.control.Label;
  *
  * @author lauri
  */
-public class FXMLProjectGuiController implements Initializable {
+public class FXMLProjectGuiController {
     
+        @FXML
+    private ResourceBundle resources;
+
+        @FXML
+    private URL location;
+
+    @FXML
+    private Label cputext;
+
+    @FXML
+    private Label disktext;
+
+    @FXML
+    private Label ostext;
+
+    @FXML
+    private Label ramtext;
+
+    @FXML
+    private Label usertext;
+
+
+        
     OperatingSystem operatingsystem = new OperatingSystem();
+    Cpu cpu = new Cpu();
+    Network network = new Network();
+    Ram ram = new Ram();
+    Processes processes = new Processes();
+    Disk disk = new Disk();
     
+    //operating system
     String osversion = operatingsystem.getOsVersion();
     String os = operatingsystem.getOS();
     String osarchitecture = operatingsystem.getOsArchitecture();
@@ -27,26 +55,60 @@ public class FXMLProjectGuiController implements Initializable {
     String userlanguage = operatingsystem.getUserLanguage();
     String homefolder = operatingsystem.getHomeFolder();
     String usercountry = operatingsystem.getUserCountry();
-    
-    
-    
-    @FXML
-    private Label label;
-    
+    //CPU
+    String cpuarchitecture = cpu.getArchitecture();
+    String identifier = cpu.getIdentifier();
+    String numberofcores = cpu.getNumberOfCores();
+    long cpufrequency = cpu.getFrequency();
+    int cpuload = cpu.getCpuLoad();
+    //network
+    String computername = network.getComputerName();
+    String userdomain = network.getUserDomain();
+    String roamingprofile = network.getRoamingProfile();
+    String macaddress = network.getMacAddress();
+    String ipaddress = network.getIpAddress();
+    String interfacename = network.getInterfaceName();
+    //process
+    String sessionname = processes.getProcessSessionName(os);
+    String explorermemusage = processes.getProcessMemUsage(os);
+    //ram
+    String ramtotalspace = Float.toString(ram.getTotalRam());
+    String ramusablespace = Float.toString(ram.getUsageRam());
+    String ramfreespace = Float.toString(ram.getFreeRam());
+    //disk
+    String disktotalspace = Long.toString(disk.getTotalSpace());
+    String diskusablespace = Long.toString(disk.getUsableSpace());
+    String diskfreespace = Long.toString(disk.getUsableSpace());
         
     
     
     
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-        
-    }
     
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-  
+
+    
+    
+    
+    @FXML
+    void initialize() {
+
+        assert cputext != null : "fx:id=\"cputext\" was not injected: check your FXML file 'FXMLProjectGui.fxml'.";
+        assert disktext != null : "fx:id=\"disktext\" was not injected: check your FXML file 'FXMLProjectGui.fxml'.";
+        assert ostext != null : "fx:id=\"ostext\" was not injected: check your FXML file 'FXMLProjectGui.fxml'.";
+        assert ramtext != null : "fx:id=\"ramtext\" was not injected: check your FXML file 'FXMLProjectGui.fxml'.";
+        assert usertext != null : "fx:id=\"usertext\" was not injected: check your FXML file 'FXMLProjectGui.fxml'.";
+
+        cputext.setText(cpuarchitecture + " " + numberofcores + " " + cpufrequency);
+        ramtext.setText(ramtotalspace);
+        ostext.setText(os + " " + osversion);
+        disktext.setText(disktotalspace);
+        usertext.setText(username);
+        
+        
 }
+
+   
+
+    
+
+
+    }
